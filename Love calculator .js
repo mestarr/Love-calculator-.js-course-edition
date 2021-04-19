80a9f1e2add6e71930ce9d4e4e92746754d46968
Stjepan Paun <67552126+mestarr@users.noreply.github.com>
2021-04-19 11:41:30 +0200

    Add files via upload

---
+++ Love calculator .js
@@ -0,0 +1,19 @@
+prompt (" Write your Name ");
+prompt (" Write a Name of your Crush ");
+
+var n = Math.random();
+n = n * 101 ;
+n = Math.floor (n) ;
+
+if ( n > 70 ) { 
+  alert ( n + " % Chance for Love " + "like Book romance ") ;
+}
+
+if (n > 30 && n <= 70) {
+   alert (n + " % Chance of Love") 
+}
+
+if ( n <= 30) {
+ alert (n + "%  Just let it go")
+}
+
